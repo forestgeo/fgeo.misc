50b07064f0b90425f2850d764a843e80f26a2358
maurolepore <maurolepore@gmail.com>
2018-12-20 17:51:06 -0500

    Add drop_twice_dead() from fgeo.tool.

--- R/imports.R
+++ R/imports.R
@@ -1,5 +1,6 @@
 #' @importFrom rlang set_names abort warn inform enquo
 #' @importFrom utils head tail
+#' @importFrom dplyr group_by ungroup mutate select filter
 NULL
 
 globalVariables(c(".data", "."))
