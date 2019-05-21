e0c6989a99766936bdd445194477ce6f70000504
Mauro Lepore <maurolepore@gmail.com>
2019-05-21 15:47:32 -0400

    Simplify (and fix) test.
    Fix test to avoid issue by which `set.seed() + sample()` gives different in different versions of R

--- tests/testthat/test-lookup.R
+++ tests/testthat/test-lookup.R
@@ -3,20 +3,17 @@ context("lookup")
 library(dplyr)
 
 test_that("returns expected spliced list", {
-  set.seed(1)
-  # Lookup table
   look <- tibble::tibble(
     old = c("a", "b"),
     new = c("apple", "banana")
   )
-  # Vector
-  x <- sample(c("a", "b", "c"), 5, replace = TRUE)
+  
   codes <- lookup(look$old, look$new)
-
   expect_type(codes, "list")
   expect_is(codes, "spliced")
-
-  recoded <- c("apple", "banana", "banana", "c", "apple")
+  
+  x <- c("a", "c", "a", "b", "a")
+  recoded <- c("apple", "c", "apple", "banana", "apple")
   expect_equal(result <- recode(x, codes), recoded)
 
   alternative <- recode(x, !!!as.list(set_names(look$new, look$old)))
